03418e0253d4a5fa343f75ea2e340a1ef4a707d2
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-09-11 16:55:23 -0700

    [pkg/common] Move env lookups into common, move values

--- pkg/common/values.go
+++ pkg/common/values.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"math/big"
+	"os"
 
 	"github.com/harmony-one/harmony/accounts/keystore"
 )
@@ -10,12 +11,25 @@ const (
 	DefaultConfigDirName               = ".hmy_cli"
 	DefaultConfigAccountAliasesDirName = "account-keys"
 	DefaultPassphrase                  = "harmony-one"
+	JSONRPCVersion                     = "2.0"
 )
 
 var (
-	ScryptN = keystore.StandardScryptN
-	ScryptP = keystore.StandardScryptP
+	ScryptN          = keystore.StandardScryptN
+	ScryptP          = keystore.StandardScryptP
+	DebugRPC         = false
+	DebugTransaction = false
 )
+
+func init() {
+	if _, enabled := os.LookupEnv("HMY_RPC_DEBUG"); enabled != false {
+		DebugRPC = true
+	}
+	if _, enabled := os.LookupEnv("HMY_TX_DEBUG"); enabled != false {
+		DebugTransaction = true
+	}
+
+}
 
 type ChainID struct {
 	Name  string
