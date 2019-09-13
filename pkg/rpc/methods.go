de6a822908ff8f1a8b83cee343ff8b7fa98daf31
Manish <manish0338@gmail.com>
2019-09-13 12:06:48 -0700

    added custom error for rpc error codes and changed error codes to camel case

--- pkg/rpc/methods.go
+++ pkg/rpc/methods.go
@@ -1,5 +1,10 @@
 package rpc
 
+import (
+	"github.com/pkg/errors"
+	"fmt"
+)
+
 // Adapted from https://github.com/harmony-one/sdk/blob/master/packages/harmony-network/src/rpcMethod/rpc.ts
 type RPCMethod string
 
@@ -76,21 +81,21 @@ var Method = rpcEnumList{
 }
 
 type rpcErrorCodeList struct {
-	RPC_INVALID_REQUEST        RPCErrorCode
-	RPC_METHOD_NOT_FOUND       RPCErrorCode
-	RPC_INVALID_PARAMS         RPCErrorCode
-	RPC_INTERNAL_ERROR         RPCErrorCode
-	RPC_PARSE_ERROR            RPCErrorCode
-	RPC_MISC_ERROR             RPCErrorCode
-	RPC_TYPE_ERROR             RPCErrorCode
-	RPC_INVALID_ADDRESS_OR_KEY RPCErrorCode
-	RPC_INVALID_PARAMETER      RPCErrorCode
-	RPC_DATABASE_ERROR         RPCErrorCode
-	RPC_DESERIALIZATION_ERROR  RPCErrorCode
-	RPC_VERIFY_ERROR           RPCErrorCode
-	RPC_VERIFY_REJECTED        RPCErrorCode
-	RPC_IN_WARMUP              RPCErrorCode
-	RPC_METHOD_DEPRECATED      RPCErrorCode
+	rpcInvalidRequest        RPCErrorCode
+	rpcMethodNotFound        RPCErrorCode
+	rpcInvalidParams         RPCErrorCode
+	rpcInternalError         RPCErrorCode
+	rpcParseError            RPCErrorCode
+	rpcMiscError             RPCErrorCode
+	rpcTypeError             RPCErrorCode
+	rpcInvalidAddressOrKey   RPCErrorCode
+	rpcInvalidParameter      RPCErrorCode
+	rpcDatabaseError         RPCErrorCode
+	rpcDeserializationError  RPCErrorCode
+	rpcVerifyError           RPCErrorCode
+	rpcVerifyRejected        RPCErrorCode
+	rpcInWarmup              RPCErrorCode
+	rpcMethodDeprecated      RPCErrorCode
 }
 
 // TODO Turn these error codes into error values in query.go
@@ -98,24 +103,84 @@ var ErrorCode = rpcErrorCodeList{
 	// Standard JSON-RPC 2.0 errors
 	// RPC_INVALID_REQUEST is internally mapped to HTTP_BAD_REQUEST (400).
 	// It should not be used for application-layer errors.
-	RPC_INVALID_REQUEST: -32600,
+	rpcInvalidRequest: -32600,
 	// RPC_METHOD_NOT_FOUND is internally mapped to HTTP_NOT_FOUND (404).
 	// It should not be used for application-layer errors.
-	RPC_METHOD_NOT_FOUND: -32601,
-	RPC_INVALID_PARAMS:   -32602,
+	rpcMethodNotFound: -32601,
+	rpcInvalidParams:   -32602,
 	// RPC_INTERNAL_ERROR should only be used for genuine errors in bitcoind
 	// (for example datadir corruption).
-	RPC_INTERNAL_ERROR: -32603,
-	RPC_PARSE_ERROR:    -32700,
+	rpcInternalError: -32603,
+	rpcParseError:    -32700,
 	// General application defined errors
-	RPC_MISC_ERROR:             -1,  // std::exception thrown in command handling
-	RPC_TYPE_ERROR:             -3,  // Unexpected type was passed as parameter
-	RPC_INVALID_ADDRESS_OR_KEY: -5,  // Invalid address or key
-	RPC_INVALID_PARAMETER:      -8,  // Invalid, missing or duplicate parameter
-	RPC_DATABASE_ERROR:         -20, // Database error
-	RPC_DESERIALIZATION_ERROR:  -22, // Error parsing or validating structure in raw format
-	RPC_VERIFY_ERROR:           -25, // General error during transaction or block submission
-	RPC_VERIFY_REJECTED:        -26, // Transaction or block was rejected by network rules
-	RPC_IN_WARMUP:              -28, // Client still warming up
-	RPC_METHOD_DEPRECATED:      -32, // RPC method is deprecated
+	rpcMiscError:              -1, // std::exception thrown in command handling
+	rpcTypeError:              -3, // Unexpected type was passed as parameter
+	rpcInvalidAddressOrKey:    -5, // Invalid address or key
+	rpcInvalidParameter:       -8, // Invalid, missing or duplicate parameter
+	rpcDatabaseError:         -20, // Database error
+	rpcDeserializationError:  -22, // Error parsing or validating structure in raw format
+	rpcVerifyError:           -25, // General error during transaction or block submission
+	rpcVerifyRejected:        -26, // Transaction or block was rejected by network rules
+	rpcInWarmup:              -28, // Client still warming up
+	rpcMethodDeprecated:      -32, // RPC method is deprecated
+}
+
+const (
+	invalidRequestError       = "Invalid Request object"
+	methodNotFoundError       = "Method not found"
+	invalidParamsError        = "Invalid method parameter(s)"
+	internalError             = "Internal JSON-RPC error"
+	parseError                = "Error while parsing the JSON text"
+	miscError                 = "std::exception thrown in command handling"
+	typeError                 = "Unexpected type was passed as parameter"
+	invalidAddressOrKeyError  = "Invalid address or key"
+	invalidParameterError     = "Invalid, missing or duplicate parameter"
+	databaseError             = "Database error"
+	deserializationError      = "Error parsing or validating structure in raw format"
+	verifyError               = "General error during transaction or block submission"
+	verifyRejectedError       = "Transaction or block was rejected by network rules"
+	rpcInWarmupError          = "Client still warming up"
+	methodDeprecatedError     = "RPC method is deprecated"
+)
+
+func ErrorNumberToError(message string,err float64) error {
+	return errors.Wrap(errors.New(message),getErrorMessage(err))
+}
+
+func getErrorMessage(err float64) string {
+	errorcode := RPCErrorCode(err)
+	switch errorcode {
+	case ErrorCode.rpcInvalidRequest:
+		return invalidRequestError
+	case ErrorCode.rpcMethodNotFound:
+		return methodNotFoundError
+	case ErrorCode.rpcInvalidParams:
+		return invalidParamsError
+	case ErrorCode.rpcInternalError:
+		return internalError
+	case ErrorCode.rpcParseError:
+		return parseError
+	case ErrorCode.rpcMiscError:
+		return miscError
+	case ErrorCode.rpcTypeError:
+		return typeError
+	case ErrorCode.rpcInvalidAddressOrKey:
+		return invalidAddressOrKeyError
+	case ErrorCode.rpcInvalidParameter:
+		return invalidParameterError
+	case ErrorCode.rpcDatabaseError:
+		return databaseError
+	case ErrorCode.rpcDeserializationError:
+		return deserializationError
+	case ErrorCode.rpcVerifyError:
+		return verifyError
+	case ErrorCode.rpcVerifyRejected:
+		return verifyRejectedError
+	case ErrorCode.rpcInWarmup:
+		return rpcInWarmupError
+	case ErrorCode.rpcMethodDeprecated:
+		return methodDeprecatedError
+	default:
+		panic(fmt.Sprintf("Error number %v not found", err))
+	}
 }
