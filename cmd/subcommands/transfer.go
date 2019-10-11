6ebbafa4a6f0908f23816273bc50808870aaf404
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-10-10 18:40:31 -0700

    [transaction] Clarify output of dry-run

--- cmd/subcommands/transfer.go
+++ cmd/subcommands/transfer.go
@@ -123,6 +123,7 @@ Create a transaction, sign it, and send off to the Harmony blockchain
 			case !dryRun && confirmWait > 0:
 				fmt.Println(common.ToJSONUnsafe(ctrlr.Receipt(), !noPrettyOutput))
 			case dryRun:
+				fmt.Println("Txn:")
 				fmt.Println(ctrlr.TransactionToJSON(!noPrettyOutput))
 				fmt.Println("RawTxn:", ctrlr.RawTransaction())
 			}
