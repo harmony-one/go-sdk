1f25a418001488044f78cbfbac31d9bf06b1f995
Manish Gangal <manish0338@gmail.com>
2019-10-11 16:56:46 -0700

    [cmd/balance][pkg/sharding] moved check balances logic to package sharding (#31)

--- pkg/sharding/structure.go
+++ pkg/sharding/structure.go
@@ -1,9 +1,13 @@
 package sharding
 
 import (
+	"bytes"
+	"fmt"
+	"math/big"
 	"encoding/json"
 
 	"github.com/harmony-one/go-sdk/pkg/rpc"
+	"github.com/harmony-one/go-sdk/pkg/common"
 )
 
 // RPCRoutes reflects the RPC endpoints of the target network across shards
@@ -25,4 +29,31 @@ func Structure(node string) ([]RPCRoutes, error) {
 	result := r{}
 	json.Unmarshal(p, &result)
 	return result.Result, nil
+}
+
+func CheckAllShards(node, addr string, noPretty bool) (string, error) {
+	var out bytes.Buffer
+	out.WriteString("[")
+	params := []interface{}{addr, "latest"}
+	s, err := Structure(node)
+	if err != nil {
+		return "", err
+	}
+	for i, shard := range s {
+		balanceRPCReply, _ := rpc.Request(rpc.Method.GetBalance, shard.HTTP, params)
+		balance, _ := balanceRPCReply["result"].(string)
+		bln, _ := big.NewInt(0).SetString(balance[2:], 16)
+		out.WriteString(fmt.Sprintf(`{"shard":%d, "amount":%s}`,
+			shard.ShardID,
+			common.ConvertBalanceIntoReadableFormat(bln),
+		))
+		if i != len(s)-1 {
+			out.WriteString(",")
+		}
+	}
+	out.WriteString("]")
+	if noPretty {
+		return out.String(), nil
+	}
+	return common.JSONPrettyFormat(out.String()), nil
 }
