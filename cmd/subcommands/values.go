e0048e7fd812616cc885122de5bacfd9300959e0
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-11-11 17:49:26 -0800

    [cookbook] Update cookbook

--- cmd/subcommands/values.go
+++ cmd/subcommands/values.go
@@ -41,12 +41,33 @@ hmy --node="https://api.s0.b.hmny.io" blockchain transaction-receipt <SOME_TX_HA
 
 %s
 hmy keys import-ks <SOME_ABSOLUTE_PATH_TO_KEYSTORE_JSON>.key
+
+%s
+hmy keys import-private-key <secp256k1_PRIVATE_KEY>
+
+%s
+hmy keys export-private-key <ACCOUNT_ADDRESS> --passphrase harmony-one
+
+%s
+hmy keys generate-bls-key --bls-file-path /tmp/file.key
+
+%s
+hmy staking create-validator --amount 10 --validator-addr one103q7qe5t2505lypvltkqtddaef5tzfxwsse4z7 \
+    --bls-pubkeys 678ec9670899bf6af85b877058bea4fc1301a5a3a376987e826e3ca150b80e3eaadffedad0fedfa111576fa76ded980c \
+        6757ebfbbc53a167e4c069cdf2beecd1428316306145c8d6d97c6c6babb3ec34e7003b3db8ccfc7d79a412aec7c68c97 \
+    --identity foo --details bar --name baz --max-change-rate 10 --max-rate 10 --max-total-delegation 10 \ 
+    --min-self-delegation 10 --rate 10 --security-contact Leo  --website harmony.one --passphrase=''
+
 `,
-		g("1. Check Balances"),
-		g("2. Check completed transaction"),
-		g("3. List local keys"),
-		g("4. Sending a transaction (add --wait-for-confirm=10 to wait 10 seconds for confirmation)"),
-		g("5. Check a completed transaction receipt"),
-		g("6. Import an existing keystore file"),
+		g("1.  Check Balances"),
+		g("2.  Check completed transaction"),
+		g("3.  List local keys"),
+		g("4.  Sending a transaction (add --wait-for-confirm=10 to wait 10 seconds for confirmation)"),
+		g("5.  Check a completed transaction receipt"),
+		g("6.  Import an existing keystore file"),
+		g("7.  Import a keystore file using a secp256k1 private key"),
+		g("8.  Export a keystore file's secp256k1 private key"),
+		g("9.  Generate a BLS key then encrypt and save the private key to the specified location"),
+		g("10. Create a new validator with a list of BLS keys"),
 	)
 )
