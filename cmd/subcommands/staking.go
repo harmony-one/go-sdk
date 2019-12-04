6bdc5dabd0e9a8b3c605abb0a9a6b5590edf2492
coolcottontail <50373379+coolcottontail@users.noreply.github.com>
2019-12-03 17:01:46 -0800

    Update staking.go

--- cmd/subcommands/staking.go
+++ cmd/subcommands/staking.go
@@ -314,10 +314,6 @@ Edit an existing validator"
 				return err
 			}
 
-			if (len(commisionRateStr) == 0) {
-				commisionRateStr = "0";
-			}
-
 			commisionRate, err := numeric.NewDecFromStr(commisionRateStr)
 			if err != nil {
 				return err
@@ -415,7 +411,7 @@ Edit an existing validator"
 		"passphrase to unlock delegator's keystore",
 	)
 
-	for _, flagName := range [...]string{"name", "identity", "website", "security-contact", "details",  "validator-addr" } {
+	for _, flagName := range [...]string{"name", "identity", "website", "security-contact", "details",  "validator-addr", "rate" } {
 		subCmdEditValidator.MarkFlagRequired(flagName)
 	}
 
