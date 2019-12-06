798e5101abca0c40fe9aba3307e4038be0f24180
Ganesha Upadhyaya <ganeshrvce@gmail.com>
2019-12-06 11:48:43 -0800

    adding clarification to help

--- scripts/hmy.sh
+++ scripts/hmy.sh
@@ -10,6 +10,9 @@ Usage: $0 [option] command
 Options:
    -d          download all the binaries
    -h          print this help
+Note: Arguments must be passed at the end for ./hmy to work correctly.
+For instance: ./hmy.sh ./hmy.sh balances <one-address> --node=https://api.s0.p.hmny.io/
+
 EOT
 }
 
