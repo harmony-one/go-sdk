72d9e9444eb7cb029bb162e2e092257f7e8ca367
Janet Liang <janet@harmony.one>
2019-12-10 12:25:14 -0800

    [#71] Change Run to RunE for consistency

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 				"Harmony (C) 2019. %v, version %v-%v (%v %v)\n",
 				path.Base(os.Args[0]), version, commit, builtBy, builtAt)
 			os.Exit(0)
-			return nils
+			return nil
 		},
 	})
 	cmd.Execute()
