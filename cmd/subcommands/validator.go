bbbd5ae9b37fdd92b5e130a39457053c01c8083f
flicker-harmony <vlad@harmony.one>
2020-02-12 21:36:10 +0300

    Add pagionation

--- cmd/subcommands/validator.go
+++ cmd/subcommands/validator.go
@@ -46,6 +46,7 @@ var (
 	}, {
 		Use:     "all-information",
 		Short:   "all validators information",
+		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			noLatest = true
 			return request(rpc.Method.GetAllValidatorInformation, []interface{}{args[0]})
