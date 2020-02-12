466810710fce7f1450c10e9f9c700309cfed65ff
flicker-harmony <vlad@harmony.one>
2020-02-12 16:57:41 +0300

    All validators information

--- cmd/subcommands/validator.go
+++ cmd/subcommands/validator.go
@@ -43,6 +43,13 @@ var (
 			noLatest = true
 			return request(rpc.Method.GetValidatorInformation, []interface{}{args[0]})
 		},
+	}, {
+		Use:     "all-information",
+		Short:   "all validators information",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			noLatest = true
+			return request(rpc.Method.GetAllValidatorInformation, []interface{}{args[0]})
+		},
 	},
 	}
 )
