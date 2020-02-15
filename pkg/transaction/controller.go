e37af50e9a5c366706fc8b1f5761186749290690
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-02-15 01:08:23 -0800

    [pkg/transaction] Implement timeout with error sink report
    * Refactor of controller was done to implement timeout
    * Add error fetch function that is tx agnostic

--- pkg/transaction/controller.go
+++ pkg/transaction/controller.go
@@ -22,6 +22,10 @@ import (
 var (
 	nanoAsDec = numeric.NewDec(denominations.Nano)
 	oneAsDec  = numeric.NewDec(denominations.One)
+
+	// ErrBadTransactionParam is returned when invalid params are given to the
+	// controller upon execution of a transaction.
+	ErrBadTransactionParam = errors.New("transaction has bad parameters")
 )
 
 type p []interface{}
@@ -30,9 +34,9 @@ type transactionForRPC struct {
 	params      map[string]interface{}
 	transaction *Transaction
 	// Hex encoded
-	signature   *string
-	receiptHash *string
-	receipt     rpc.Reply
+	signature       *string
+	transactionHash *string
+	receipt         rpc.Reply
 }
 
 type sender struct {
@@ -42,7 +46,8 @@ type sender struct {
 
 // Controller drives the transaction signing process
 type Controller struct {
-	failure           error
+	executionError    error
+	transactionErrors Errors
 	messenger         rpc.T
 	sender            sender
 	transactionForRPC transactionForRPC
@@ -58,25 +63,23 @@ type behavior struct {
 
 // NewController initializes a Controller, caller can control behavior via options
 func NewController(
-	handler rpc.T,
-	senderKs *keystore.KeyStore,
-	senderAcct *accounts.Account,
-	chain common.ChainID,
-	options ...func(*Controller)) *Controller {
-
+	handler rpc.T, senderKs *keystore.KeyStore,
+	senderAcct *accounts.Account, chain common.ChainID,
+	options ...func(*Controller),
+) *Controller {
 	txParams := make(map[string]interface{})
 	ctrlr := &Controller{
-		failure:   nil,
-		messenger: handler,
+		executionError: nil,
+		messenger:      handler,
 		sender: sender{
 			ks:      senderKs,
 			account: senderAcct,
 		},
 		transactionForRPC: transactionForRPC{
-			params:      txParams,
-			signature:   nil,
-			receiptHash: nil,
-			receipt:     nil,
+			params:          txParams,
+			signature:       nil,
+			transactionHash: nil,
+			receipt:         nil,
 		},
 		chain:    chain,
 		Behavior: behavior{false, Software, 0},
@@ -87,89 +90,7 @@ func NewController(
 	return ctrlr
 }
 
-func (C *Controller) verifyBalance() {
-	if C.failure != nil {
-		return
-	}
-	balanceRPCReply, err := C.messenger.SendRPC(
-		rpc.Method.GetBalance,
-		p{address.ToBech32(C.sender.account.Address), "latest"},
-	)
-	if err != nil {
-		C.failure = err
-		return
-	}
-	currentBalance, _ := balanceRPCReply["result"].(string)
-	bal, _ := new(big.Int).SetString(currentBalance[2:], 16)
-	balance := numeric.NewDecFromBigInt(bal)
-	gasAsDec := C.transactionForRPC.params["gas-price"].(numeric.Dec)
-	gasAsDec = gasAsDec.Mul(numeric.NewDec(int64(C.transactionForRPC.params["gas-limit"].(uint64))))
-	total := C.transactionForRPC.params["transfer-amount"].(numeric.Dec).Add(gasAsDec)
-
-	if total.GT(balance) {
-		b := balance.Quo(oneAsDec)
-		t := total.Quo(oneAsDec)
-		C.failure = fmt.Errorf(
-			"insufficient balance of %s in shard %d for the requested transfer of %s", b.String(), C.transactionForRPC.params["from-shard"].(uint32), t.String(),
-		)
-	}
-}
-
-func (C *Controller) sendSignedTx() {
-	if C.failure != nil || C.Behavior.DryRun {
-		return
-	}
-	reply, err := C.messenger.SendRPC(rpc.Method.SendRawTransaction, p{C.transactionForRPC.signature})
-	if err != nil {
-		C.failure = err
-		return
-	}
-	r, _ := reply["result"].(string)
-	C.transactionForRPC.receiptHash = &r
-}
-
-func (C *Controller) setIntrinsicGas(gasLimit uint64) {
-	if C.failure != nil {
-		return
-	}
-	C.transactionForRPC.params["gas-limit"] = gasLimit
-}
-
-func (C *Controller) setGasPrice(gasPrice numeric.Dec) {
-	if C.failure != nil {
-		return
-	}
-	C.transactionForRPC.params["gas-price"] = gasPrice.Mul(nanoAsDec)
-}
-
-func (C *Controller) setAmount(amount numeric.Dec) {
-	amt := amount.Mul(oneAsDec)
-	C.transactionForRPC.params["transfer-amount"] = amt
-}
-
-func (C *Controller) setReceiver(receiver string) {
-	C.transactionForRPC.params["receiver"] = address.Parse(receiver)
-}
-
-func (C *Controller) setNewTransactionWithDataAndGas(i string) {
-	if C.failure != nil {
-		return
-	}
-
-	tx := NewTransaction(
-		C.transactionForRPC.params["nonce"].(uint64),
-		C.transactionForRPC.params["gas-limit"].(uint64),
-		C.transactionForRPC.params["receiver"].(address.T),
-		C.transactionForRPC.params["from-shard"].(uint32),
-		C.transactionForRPC.params["to-shard"].(uint32),
-		C.transactionForRPC.params["transfer-amount"].(numeric.Dec),
-		C.transactionForRPC.params["gas-price"].(numeric.Dec),
-		[]byte(i),
-	)
-	C.transactionForRPC.transaction = tx
-}
-
-// TransactionToJSON dumps JSON rep
+// TransactionToJSON dumps JSON representation
 func (C *Controller) TransactionToJSON(pretty bool) string {
 	r, _ := C.transactionForRPC.transaction.MarshalJSON()
 	if pretty {
@@ -183,14 +104,103 @@ func (C *Controller) RawTransaction() string {
 	return *C.transactionForRPC.signature
 }
 
+func (C *Controller) TransactionHash() *string {
+	return C.transactionForRPC.transactionHash
+}
+
+func (C *Controller) Receipt() rpc.Reply {
+	return C.transactionForRPC.receipt
+}
+
+func (C *Controller) TransactionErrors() Errors {
+	return C.transactionErrors
+}
+
+func (C *Controller) setShardIDs(fromShard, toShard int) {
+	if C.executionError != nil {
+		return
+	}
+	C.transactionForRPC.params["from-shard"] = uint32(fromShard)
+	C.transactionForRPC.params["to-shard"] = uint32(toShard)
+}
+
+func (C *Controller) setIntrinsicGas(gasLimit uint64) {
+	if C.executionError != nil {
+		return
+	}
+	C.transactionForRPC.params["gas-limit"] = gasLimit
+}
+
+func (C *Controller) setGasPrice(gasPrice numeric.Dec) {
+	if C.executionError != nil {
+		return
+	}
+	C.transactionForRPC.params["gas-price"] = gasPrice.Mul(nanoAsDec)
+}
+
+func (C *Controller) setAmount(amount numeric.Dec) {
+	if C.executionError != nil {
+		return
+	}
+	balanceRPCReply, err := C.messenger.SendRPC(
+		rpc.Method.GetBalance,
+		p{address.ToBech32(C.sender.account.Address), "latest"},
+	)
+	if err != nil {
+		C.executionError = err
+		return
+	}
+	currentBalance, _ := balanceRPCReply["result"].(string)
+	bal, _ := new(big.Int).SetString(currentBalance[2:], 16)
+	balance := numeric.NewDecFromBigInt(bal)
+	gasAsDec := C.transactionForRPC.params["gas-price"].(numeric.Dec)
+	gasAsDec = gasAsDec.Mul(numeric.NewDec(int64(C.transactionForRPC.params["gas-limit"].(uint64))))
+	amountInAtto := amount.Mul(oneAsDec)
+	total := amountInAtto.Add(gasAsDec)
+
+	if total.GT(balance) {
+		balanceInOne := balance.Quo(oneAsDec)
+		C.executionError = ErrBadTransactionParam
+		errorMsg := fmt.Sprintf(
+			"insufficient balance of %s in shard %d for the requested transfer of %s",
+			balanceInOne.String(), C.transactionForRPC.params["from-shard"].(uint32), amount.String(),
+		)
+		C.transactionErrors = append(C.transactionErrors, &Error{
+			ErrMessage:           &errorMsg,
+			TimestampOfRejection: time.Now().Unix(),
+		})
+	}
+	C.transactionForRPC.params["transfer-amount"] = amountInAtto
+}
+
+func (C *Controller) setReceiver(receiver string) {
+	C.transactionForRPC.params["receiver"] = address.Parse(receiver)
+}
+
+func (C *Controller) setNewTransactionWithDataAndGas(data []byte) {
+	if C.executionError != nil {
+		return
+	}
+	C.transactionForRPC.transaction = NewTransaction(
+		C.transactionForRPC.params["nonce"].(uint64),
+		C.transactionForRPC.params["gas-limit"].(uint64),
+		C.transactionForRPC.params["receiver"].(address.T),
+		C.transactionForRPC.params["from-shard"].(uint32),
+		C.transactionForRPC.params["to-shard"].(uint32),
+		C.transactionForRPC.params["transfer-amount"].(numeric.Dec),
+		C.transactionForRPC.params["gas-price"].(numeric.Dec),
+		data,
+	)
+}
+
 func (C *Controller) signAndPrepareTxEncodedForSending() {
-	if C.failure != nil {
+	if C.executionError != nil {
 		return
 	}
 	signedTransaction, err :=
 		C.sender.ks.SignTx(*C.sender.account, C.transactionForRPC.transaction, C.chain.Value)
 	if err != nil {
-		C.failure = err
+		C.executionError = err
 		return
 	}
 	C.transactionForRPC.transaction = signedTransaction
@@ -204,75 +214,85 @@ func (C *Controller) signAndPrepareTxEncodedForSending() {
 	}
 }
 
-func (C *Controller) setShardIDs(fromShard, toShard int) {
-	if C.failure != nil {
-		return
-	}
-	C.transactionForRPC.params["from-shard"] = uint32(fromShard)
-	C.transactionForRPC.params["to-shard"] = uint32(toShard)
-}
-
-func (C *Controller) ReceiptHash() *string {
-	return C.transactionForRPC.receiptHash
-}
-
-func (C *Controller) Receipt() rpc.Reply {
-	return C.transactionForRPC.receipt
-}
-
 func (C *Controller) hardwareSignAndPrepareTxEncodedForSending() {
-	if C.failure != nil {
+	if C.executionError != nil {
 		return
 	}
 	enc, signerAddr, err := ledger.SignTx(C.transactionForRPC.transaction, C.chain.Value)
 	if err != nil {
-		C.failure = err
+		C.executionError = err
 		return
 	}
 	if strings.Compare(signerAddr, address.ToBech32(C.sender.account.Address)) != 0 {
-		C.failure = errors.New("signature verification failed : sender address doesn't match with ledger hardware addresss")
+		C.executionError = ErrBadTransactionParam
+		errorMsg := "signature verification failed : sender address doesn't match with ledger hardware addresss"
+		C.transactionErrors = append(C.transactionErrors, &Error{
+			ErrMessage:           &errorMsg,
+			TimestampOfRejection: time.Now().Unix(),
+		})
 		return
 	}
 	hexSignature := hexutil.Encode(enc)
 	C.transactionForRPC.signature = &hexSignature
 }
 
+func (C *Controller) sendSignedTx() {
+	if C.executionError != nil || C.Behavior.DryRun {
+		return
+	}
+	reply, err := C.messenger.SendRPC(rpc.Method.SendRawTransaction, p{C.transactionForRPC.signature})
+	if err != nil {
+		C.executionError = err
+		return
+	}
+	r, _ := reply["result"].(string)
+	C.transactionForRPC.transactionHash = &r
+}
+
 func (C *Controller) txConfirmation() {
-	if C.failure != nil || C.Behavior.DryRun {
+	if C.executionError != nil || C.Behavior.DryRun {
 		return
 	}
 	if C.Behavior.ConfirmationWaitTime > 0 {
-		receipt := *C.ReceiptHash()
+		txHash := *C.TransactionHash()
 		start := int(C.Behavior.ConfirmationWaitTime)
 		for {
 			if start < 0 {
+				transactionErrors, err := GetError(txHash, C.messenger)
+				if err != nil {
+					C.executionError = err
+					return
+				}
+				C.transactionErrors = append(C.transactionErrors, transactionErrors...)
+				C.executionError = fmt.Errorf("could not confirm transaction after %d seconds", C.Behavior.ConfirmationWaitTime)
 				return
 			}
-			r, _ := C.messenger.SendRPC(rpc.Method.GetTransactionReceipt, p{receipt})
+			r, _ := C.messenger.SendRPC(rpc.Method.GetTransactionReceipt, p{txHash})
 			if r["result"] != nil {
 				C.transactionForRPC.receipt = r
 				return
 			}
-			time.Sleep(time.Second * 2)
-			start = start - 2
+			time.Sleep(time.Second)
+			start -= 1
 		}
 	}
 }
 
-// ExecuteTransaction is the single entrypoint to execute a transaction.
+// ExecuteTransaction is the single entrypoint to execute a plain transaction.
 // Each step in transaction creation, execution probably includes a mutation
-// Each becomes a no-op if failure occured in any previous step
+// Each becomes a no-op if executionError occurred in any previous step
 func (C *Controller) ExecuteTransaction(
-	to, inputData string,
-	amount, gasPrice numeric.Dec, nonce, gasLimit uint64,
-	fromShard, toShard int,
+	nonce, gasLimit uint64,
+	to string,
+	shardID, toShardID int,
+	amount, gasPrice numeric.Dec,
+	inputData []byte,
 ) error {
 	// WARNING Order of execution matters
-	C.setShardIDs(fromShard, toShard)
+	C.setShardIDs(shardID, toShardID)
 	C.setIntrinsicGas(gasLimit)
 	C.setGasPrice(gasPrice)
 	C.setAmount(amount)
-	C.verifyBalance()
 	C.setReceiver(to)
 	C.transactionForRPC.params["nonce"] = nonce
 	C.setNewTransactionWithDataAndGas(inputData)
@@ -284,5 +304,7 @@ func (C *Controller) ExecuteTransaction(
 	}
 	C.sendSignedTx()
 	C.txConfirmation()
-	return C.failure
+	return C.executionError
 }
+
+// TODO: add logic to create staking transactions in the SDK.
