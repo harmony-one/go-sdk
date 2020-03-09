24e9c71168ab3e8b2871f9c308955a55b2bd111b
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-03-08 23:21:02 -0700

    [cmd] Fix getting passphrase from file #166

--- cmd/subcommands/keys.go
+++ cmd/subcommands/keys.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/harmony-one/go-sdk/pkg/account"
@@ -49,7 +50,8 @@ func getPassphrase() (string, error) {
 		if err != nil {
 			return "", err
 		}
-		return string(dat), nil
+		pw := strings.TrimSuffix(string(dat), "\n")
+		return pw, nil
 	} else if userProvidesPassphrase {
 		fmt.Println("Enter passphrase:")
 		pass, err := terminal.ReadPassword(int(os.Stdin.Fd()))
@@ -75,7 +77,7 @@ func keysSub() []*cobra.Command {
 	cmdList := &cobra.Command{
 		Use:   "list",
 		Short: "List all the local accounts",
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if useLedgerWallet {
 				ledger.ProcessAddressCommand()
 				return nil
@@ -88,7 +90,7 @@ func keysSub() []*cobra.Command {
 	cmdLocation := &cobra.Command{
 		Use:   "location",
 		Short: "Show where `hmy` keeps accounts & their keys",
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println(store.DefaultLocation())
 			return nil
 		},
@@ -98,7 +100,7 @@ func keysSub() []*cobra.Command {
 		Use:   "add <ACCOUNT_NAME>",
 		Short: "Create a new keystore key",
 		Args:  cobra.ExactArgs(1),
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if store.DoesNamedAccountExist(args[0]) {
 				return fmt.Errorf("account %s already exists", args[0])
 			}
@@ -141,7 +143,7 @@ func keysSub() []*cobra.Command {
 		Use:   "remove <ACCOUNT_NAME>",
 		Short: "Remove a key from the keystore",
 		Args:  cobra.ExactArgs(1),
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := account.RemoveAccount(args[0]); err != nil {
 				return err
 			}
@@ -152,7 +154,7 @@ func keysSub() []*cobra.Command {
 	cmdMnemonic := &cobra.Command{
 		Use:   "mnemonic",
 		Short: "Compute the bip39 mnemonic for some input entropy",
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println(mnemonic.Generate())
 			return nil
 		},
@@ -162,28 +164,28 @@ func keysSub() []*cobra.Command {
 		Use:   "recover-from-mnemonic [ACCOUNT_NAME]",
 		Short: "Recover account from mnemonic",
 		Args:  cobra.ExactArgs(1),
-		RunE:  func (cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if store.DoesNamedAccountExist(args[0]) {
-	      return fmt.Errorf("account %s already exists", args[0])
-	    }
-	    passphrase, err := getPassphrase()
-	    if err != nil {
-	      return err
-	    }
-	    acc := account.Creation{
-	      Name:       args[0],
-	      Passphrase: passphrase,
-	    }
-	    fmt.Println("Enter mnemonic to recover keys from")
-	    scanner := bufio.NewScanner(os.Stdin)
-	    scanner.Scan()
-	    m := scanner.Text()
-	    if !bip39.IsMnemonicValid(m) {
-	      return mnemonic.InvalidMnemonic
-	    }
-	    acc.Mnemonic = m
-	    if err := account.CreateNewLocalAccount(&acc); err != nil {
-	      return err
+				return fmt.Errorf("account %s already exists", args[0])
+			}
+			passphrase, err := getPassphrase()
+			if err != nil {
+				return err
+			}
+			acc := account.Creation{
+				Name:       args[0],
+				Passphrase: passphrase,
+			}
+			fmt.Println("Enter mnemonic to recover keys from")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			m := scanner.Text()
+			if !bip39.IsMnemonicValid(m) {
+				return mnemonic.InvalidMnemonic
+			}
+			acc.Mnemonic = m
+			if err := account.CreateNewLocalAccount(&acc); err != nil {
+				return err
 			}
 			fmt.Println("Successfully recovered account from mnemonic!")
 			addr, _ := store.AddressFromAccountName(acc.Name)
@@ -191,14 +193,14 @@ func keysSub() []*cobra.Command {
 			return nil
 		},
 	}
-  cmdRecoverMnemonic.Flags().BoolVar(&userProvidesPassphrase, "passphrase", false, ppPrompt)
-  cmdRecoverMnemonic.Flags().StringVar(&passphraseFilePath, "passphrase-file", "", "path to a file containing the passphrase")
+	cmdRecoverMnemonic.Flags().BoolVar(&userProvidesPassphrase, "passphrase", false, ppPrompt)
+	cmdRecoverMnemonic.Flags().StringVar(&passphraseFilePath, "passphrase-file", "", "path to a file containing the passphrase")
 
 	cmdImportKS := &cobra.Command{
 		Use:   "import-ks <KEYSTORE_FILE_PATH> [ACCOUNT_NAME]",
 		Args:  cobra.RangeArgs(1, 2),
 		Short: "Import an existing keystore key",
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			userName := ""
 			if len(args) == 2 {
 				userName = args[1]
@@ -224,7 +226,7 @@ func keysSub() []*cobra.Command {
 		Use:   "import-private-key <secp256k1_PRIVATE_KEY> [ACCOUNT_NAME]",
 		Short: "Import an existing keystore key (only accept secp256k1 private keys)",
 		Args:  cobra.RangeArgs(1, 2),
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			userName := ""
 			if len(args) == 2 {
 				userName = args[1]
@@ -250,7 +252,7 @@ func keysSub() []*cobra.Command {
 		Short:   "Export the secp256k1 private key",
 		Args:    cobra.ExactArgs(1),
 		PreRunE: validateAddress,
-		RunE:    func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			passphrase, err := getPassphrase()
 			if err != nil {
 				return err
@@ -266,7 +268,7 @@ func keysSub() []*cobra.Command {
 		Short:   "Export the keystore file contents",
 		Args:    cobra.ExactArgs(1),
 		PreRunE: validateAddress,
-		RunE:    func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			passphrase, err := getPassphrase()
 			if err != nil {
 				return err
@@ -280,7 +282,7 @@ func keysSub() []*cobra.Command {
 	cmdGenerateBlsKey := &cobra.Command{
 		Use:   "generate-bls-key",
 		Short: "Generate bls keys then encrypt and save the private key with a requested passphrase",
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			passphrase, err := getPassphrase()
 			if err != nil {
 				return err
@@ -297,7 +299,7 @@ func keysSub() []*cobra.Command {
 		Use:   "recover-bls-key <ABSOLUTE_PATH_BLS_KEY>",
 		Short: "Recover bls keys from an encrypted bls key file",
 		Args:  cobra.ExactArgs(1),
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			passphrase, err := getPassphrase()
 			if err != nil {
 				return err
@@ -312,7 +314,7 @@ func keysSub() []*cobra.Command {
 		Use:   "save-bls-key <PRIVATE_BLS_KEY>",
 		Short: "Encrypt and save the bls private key with a requested passphrase",
 		Args:  cobra.ExactArgs(1),
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			passphrase, err := getPassphrase()
 			if err != nil {
 				return err
@@ -329,7 +331,7 @@ func keysSub() []*cobra.Command {
 		Use:   "get-public-bls-key <PRIVATE_BLS_KEY>",
 		Short: "Get the public bls key associated with the provided private bls key",
 		Args:  cobra.ExactArgs(1),
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			return keys.GetPublicBlsKey(args[0])
 		},
 	}
@@ -343,7 +345,7 @@ func init() {
 		Use:   "keys",
 		Short: "Add or view local private keys",
 		Long:  "Manage your local keys",
-		RunE:  func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			cmd.Help()
 			return nil
 		},
