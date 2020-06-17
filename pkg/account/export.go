01b51fab97d35c6b45eb3b3f2611c9f307796872
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-06-17 01:39:35 -0700

    [account] Add VerifyPassphrase
    * Add checks for AddressNotFound

--- pkg/account/export.go
+++ pkg/account/export.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"errors"
 	"fmt"
 	"path/filepath"
 
@@ -8,36 +9,55 @@ import (
 	"github.com/harmony-one/harmony/accounts"
 )
 
+var (
+	ErrAddressNotFound = errors.New("account was not found in keystore")
+)
+
 func ExportPrivateKey(address, passphrase string) error {
 	ks := store.FromAddress(address)
-	allAccounts := ks.Accounts()
-	for _, account := range allAccounts {
-		_, key, err := ks.GetDecryptedKey(accounts.Account{Address: account.Address}, passphrase)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%064x\n", key.PrivateKey.D)
+	if ks == nil {
+		return ErrAddressNotFound
 	}
+	account := ks.Accounts()[0]
+	_, key, err := ks.GetDecryptedKey(accounts.Account{Address: account.Address}, passphrase)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%064x\n", key.PrivateKey.D)
 	return nil
+}
+
+func VerifyPassphrase(address, passphrase string) (bool, error) {
+	ks := store.FromAddress(address)
+	if ks == nil {
+		return false, ErrAddressNotFound
+	}
+	account := ks.Accounts()[0]
+	_, _, err := ks.GetDecryptedKey(accounts.Account{Address: account.Address}, passphrase)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 func ExportKeystore(address, path, passphrase string) (string, error) {
 	ks := store.FromAddress(address)
-	allAccounts := ks.Accounts()
+	if ks == nil {
+		return "", ErrAddressNotFound
+	}
+	account := ks.Accounts()[0]
 	dirPath, err := filepath.Abs(path)
 	if err != nil {
 		return "", err
 	}
 	outFile := filepath.Join(dirPath, fmt.Sprintf("%s.key", address))
-	for _, account := range allAccounts {
-		keyFile, err := ks.Export(accounts.Account{Address: account.Address}, passphrase, passphrase)
-		if err != nil {
-			return "", err
-		}
-		e := writeToFile(outFile, string(keyFile))
-		if e != nil {
-			return "", e
-		}
+	keyFile, err := ks.Export(accounts.Account{Address: account.Address}, passphrase, passphrase)
+	if err != nil {
+		return "", err
+	}
+	e := writeToFile(outFile, string(keyFile))
+	if e != nil {
+		return "", e
 	}
 	return outFile, nil
 }
