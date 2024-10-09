6c0174e16e3fa23ca180d34bed9848608dc6edf7
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-10-09 16:36:04 -0700

    logic update for recovering keys using mnemonics
    index flag when recovering keys

--- pkg/keys/mnemonic_test.go
+++ pkg/keys/mnemonic_test.go
@@ -9,10 +9,11 @@ const (
 	index      = 0
 	publicKey  = "0x030b64624e60c6e6758711fdf00f7e873f40a22e647a6918ba73807fab194d09ba"
 	privateKey = "0xda4bc68857640103942ce7dd22a9fcdb96f3cfe0380254e81352a94ac8262ed2"
+	coinType   = 1023
 )
 
 func TestMnemonic(t *testing.T) {
-	private, public := FromMnemonicSeedAndPassphrase(phrase, index)
+	private, public := FromMnemonicSeedAndPassphrase(phrase, index, coinType)
 	sk, pkCompressed := func() (string, string) {
 		dump := EncodeHex(private, public)
 		return dump.PrivateKey, dump.PublicKeyCompressed
